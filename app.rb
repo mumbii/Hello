8391e297c7382beded826dd5fef5fca2f5315390
JamesHanks <james.ra.hanks@gmail.com>
2018-05-17 15:06:28 +0100

    Privacy Policy Page

--- app.rb
+++ app.rb
@@ -5,103 +5,108 @@ require 'mailgun-ruby'
 require 'rack-flash'
 
 class HelloLamppostWebsite < Sinatra::Base
-  enable :sessions
-  use Rack::Flash
+	enable :sessions
+	use Rack::Flash
 
-  configure do
-    I18n.load_path = Dir[File.join(settings.root, 'config', 'locales', '*.yml')]
-    I18n.default_locale = :en
-    I18n.backend.load_translations
-  end
+	configure do
+		I18n.load_path = Dir[File.join(settings.root, 'config', 'locales', '*.yml')]
+		I18n.default_locale = :en
+		I18n.backend.load_translations
+	end
 
-  helpers do
+	helpers do
 
-    def link_html_class(path)
-      request.path_info.start_with?(path) ? 'current' : ''
-    end
+		def link_html_class(path)
+			request.path_info.start_with?(path) ? 'current' : ''
+		end
 
-    def locale_path(path)
-      return "/#{I18n.locale}#{path}"
-    end
+		def locale_path(path)
+			return "/#{I18n.locale}#{path}"
+		end
 
-    def code_parser(code)
+		def code_parser(code)
 
-      code = code.to_s
+			code = code.to_s
 
-      words = code.split(/\s+/)
+			words = code.split(/\s+/)
 
-      if words.length >= 2
-        {
-          object_type: (words[0..-2].join('_').downcase),
-          object_code: words[-1].gsub("#", '')
-        }
-      else
-        nil
-      end
+			if words.length >= 2
+				{
+					object_type: (words[0..-2].join('_').downcase),
+					object_code: words[-1].gsub("#", '')
+				}
+			else
+				nil
+			end
 
-    end
+		end
 
-  end
+	end
 
-  # before /.*/ do
-  #
-  #   matches = request.path_info.match(/\A\/(sv|ru|en)(.*)/)
-  #
-  #   if matches
-  #     I18n.locale       = matches[1]
-  #     request.path_info = matches[2]
-  #   else
-  #     redirect to('/en' + request.path_info)
-  #   end
-  # end
+	# before /.*/ do
+	#
+	#   matches = request.path_info.match(/\A\/(sv|ru|en)(.*)/)
+	#
+	#   if matches
+	#     I18n.locale       = matches[1]
+	#     request.path_info = matches[2]
+	#   else
+	#     redirect to('/en' + request.path_info)
+	#   end
+	# end
 
-  get '/survey' do
-    redirect "https://docs.google.com/forms/d/e/1FAIpQLSdm5phYBgjy6y6VkQTrpc_8OWV4CDIeP5zIU4laBS5cPblRqA/viewform"
-  end
+	get '/survey' do
+		redirect "https://docs.google.com/forms/d/e/1FAIpQLSdm5phYBgjy6y6VkQTrpc_8OWV4CDIeP5zIU4laBS5cPblRqA/viewform"
+	end
 
-  get '/' do
-    @page_title = I18n.t(:home_title)
-    erb :index
-  end
+	get '/' do
+		@page_title = I18n.t(:home_title)
+		erb :index
+	end
 
-  get '/news' do
-    @page_title = I18n.t(:home_title)
-    erb :news
-  end
+	get '/news' do
+		@page_title = I18n.t(:home_title)
+		erb :news
+	end
 
-  post '/contact' do
+	get '/privacy' do
+		@page_title = I18n.t(:home_title)
+		erb :privacy
+	end
 
-  # First, instantiate the Mailgun Client with your API key
-  mg_client = Mailgun::Client.new 'key-dee4e566f732ad64442a493985063a2e'
+	post '/contact' do
 
-  # Define your message parameters
+	# First, instantiate the Mailgun Client with your API key
+	mg_client = Mailgun::Client.new 'key-dee4e566f732ad64442a493985063a2e'
 
-    email = params['email']
-    message = params['message']
-    subject = params['subject']
+	# Define your message parameters
 
-    message_params =  { from: email,
-                        to:   'tiernan.mines@panstudio.co.uk',
-                        subject: subject,
-                        text:    message
-                      }
-    # Send your message through the client
-    mg_client.send_message 'sandbox1078ce278f404e358edd42b8db68c285.mailgun.org', message_params
+	email = params['email']
+	message = params['message']
+	subject = params['subject']
 
-    flash[:ok] = "Your Message Has Been Sent!"
-    erb :index
+	message_params =  { from: email,
+		to:   'tiernan.mines@panstudio.co.uk',
+		subject: subject,
+		text:    message
+	}
+		# Send your message through the client
+		mg_client.send_message 'sandbox1078ce278f404e358edd42b8db68c285.mailgun.org', message_params
 
-  end
+		flash[:ok] = "Your Message Has Been Sent!"
+		erb :index
 
-  get '/questions/random' do
-    cache_control :public, max_age: 0
-    url = "http://hello-lamp-post-api.herokuapp.com/questions/random?location_id=10&except=292&locale=sv"
-    open(url).read
-  end
+	end
 
-  get '/' do
-    @page_title = "Hello Lamp Post Malmo"
-    erb :index
-  end
+	get '/questions/random' do
+		cache_control :public, max_age: 0
+		url = "http://hello-lamp-post-api.herokuapp.com/questions/random?location_id=10&except=292&locale=sv"
+		open(url).read
+	end
+
+	get '/' do
+		@page_title = "Hello Lamp Post Malmo"
+		erb :index
+	end
 
 end
